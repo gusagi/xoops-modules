153d4d8651c03b70c6cee4971e200ebe9ecda4ba
gusagi <gusagi@gmail.com>
2010-03-20 01:23:47 +0900

    Added mobile theme 'wmb_default'

---
+++ html/themes/wmb_default/manifesto.ini.php
@@ -0,0 +1,21 @@
+<?php
+/**
+
+[Manifesto]
+Name="wmb_default"
+Depends=Legacy_RenderSystem,legacy
+Url="http://www.gusagi.com/"
+Version="0.1"
+
+[Theme]
+RenderSystem=Legacy_WizMobileRenderSystem
+Format="Default mobile style with WizMobile module"
+Author="Makoto Hashiguchi a.k.a. gusagi<gusagi@gusagi.com>"
+ScreenShot="screenshot.png"
+Description="Theme for access from japanese mobile phone."
+W3C=NO
+
+Licence="GPL"
+
+*/
+?>
